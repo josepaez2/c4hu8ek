10d1917c423620253a80e9f9950e89b84971334e
josepaez2 <josepaez_2@hotmail.com>
2017-09-15 13:25:28 -0500

    API Security

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,6 +1,19 @@
+require 'httparty'
+
 class Api::V1::PinsController < ApplicationController
+before_action :authenticate_user, only: [:create]
+
+# buscar al usuario y ver si está autenticado
+# device tiene current user verlo en costsos
+
   def index
-    render json: Pin.all.order('created_at DESC')
+    HTTParty.post('http://localhost:3000/api/v1/pins', body: { user_id: 1 , title: "QPMA", image_url: "https://s3.amazonaws.com/makeitreal/pins/lamborghini.jpg"}.to_json, headers: { 'Content-Type' => 'application/json', 'X-User-Email'=> 'josepaez_2@hotmail.com', 'X-Api-Token'=> '20d8cf670e88edb69e02f93ed2aa493d'})
+    # puts "-------------------"
+    # puts  @current_user = current_user
+    # puts "-------------------"
+    # puts  @current_user = current_user
+    # puts "-------------------"
+    # render json: Pin.all.order('created_at DESC')
   end
 
   def create
@@ -11,6 +24,22 @@ class Api::V1::PinsController < ApplicationController
       render json: { errors: pin.errors }, status: 422
     end
   end
+
+
+# body: { user_id: 1 , title: "QPMA", image_url: "https://s3.amazonaws.com/makeitreal/pins/lamborghini.jpg"}.to_json, headers: { 'Content-Type' => 'application/json', 'X-User-Email'=> 'josepaez_2@hotmail.com', 'X-Api-Token'=> '20d8cf670e88edb69e02f93ed2aa493d'})
+  # Confirms a valid user.
+  def authenticate_user
+    @current_user ||= User.find(body[:user_id])
+    # @current_user ||= User.find_by(id: body[:user_id])
+    puts "@current_user"
+    puts @current_user
+    puts @current_user.email
+    puts headers[':X-User-Email']
+    puts @current_user.api_token
+    puts headers['X-Api-Token']
+    end
+  end
+
 
   private
     def pin_params
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,4 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
+  # id, user_id, title, created_at, updated_at, image_url
 end
