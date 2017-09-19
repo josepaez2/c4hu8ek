c16bc3281ed34bda4b5bf6fac948b4b564e78ff8
josepaez2 <josepaez_2@hotmail.com>
2017-09-19 14:51:35 -0500

    Final changes, removed extra controller bug

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,12 +1,28 @@
-require 'httparty'
+# require 'httparty'
 
 class Api::V1::PinsController < ApplicationController
+before_action :authenticate_user!, except: [:index]
 skip_before_action :verify_authenticity_token
 # before_action :authenticate_user, only: [:create]
 
   def index
+    email = request.headers['HTTP_X_USER_EMAIL']
+    api_token = request.headers['HTTP_X_API_TOKEN']
+    if @current_user = User.find_by(email: email)
+      if @current_user.api_token == api_token
+          render json: Pin.all.order('created_at DESC'), status: 201
+      else
+        # pin.errors.add :title, "must be a valid password"
+        # render json: { errors: pin.errors }, status: 401
+        render json: { errors: {password:"must be a valid password"}}, status: 401
+      end
+    else
+      # pin.errors.add :title, "must be a valid email"
+      # render json: { errors: pin.errors }, status: 401
+      render json: { errors: {email:"must be a valid email"}}, status: 401
+    end
     # HTTParty.post('http://localhost:3000/api/v1/pins', body: { title: "QPMA", image_url: "https://s3.amazonaws.com/makeitreal/pins/lamborghini.jpg"}.to_json, headers: { 'Content-Type' => 'application/json', 'X-User-Email'=> 'josepaez_2@hotmail.com', 'X-Api-Token'=> '20d8cf670e88edb69e02f93ed2aa493d'})
-    render json: Pin.all.order('created_at DESC')
+    # HTTParty.get('http://localhost:3000/api/v1/pins', body: { title: "QPMA", image_url: "https://s3.amazonaws.com/makeitreal/pins/lamborghini.jpg"}.to_json, headers: { 'Content-Type' => 'application/json', 'X-User-Email'=> 'josepaez_2@hotmail.com', 'X-Api-Token'=> '20d8cf670e88edb69e02f93ed2aa493d'})
   end
 
   def create
@@ -18,12 +34,14 @@ skip_before_action :verify_authenticity_token
         pin.save
         render json: pin, status: 201
       else
-        pin.errors.add :title, "must be a valid password"
-        render json: { errors: pin.errors }, status: 401
+        # pin.errors.add :title, "must be a valid password"
+        # render json: { errors: pin.errors }, status: 401
+        render json: { errors: {password:"must be a valid password"}}, status: 401
       end
     else
-      pin.errors.add :title, "must be a valid email"
-      render json: { errors: pin.errors }, status: 401
+      # pin.errors.add :title, "must be a valid email"
+      # render json: { errors: pin.errors }, status: 401
+      render json: { errors: {email:"must be a valid email"}}, status: 401
     end
   end
 
