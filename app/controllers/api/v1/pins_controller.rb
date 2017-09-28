9c8fc93cf4941f8f2881eba0c108446233093e44
josepaez2 <josepaez_2@hotmail.com>
2017-09-28 13:01:32 -0500

    Final changes, corrected before action method

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -16,7 +16,7 @@ skip_before_action :verify_authenticity_token
   def create
     pin = Pin.new(pin_params)
         pin.save
-        render json: pin, status: 200
+        render json: pin, status: 201
   end
 
   def destroy
